78a739cfa889fabffff296406b101a0e1b1f779b
beku026 <beksultanbakybekov@gmail.com>
2022-07-12 23:09:30 +0600

    added task twitter-2

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const form = document.querySelector(".form");
 const komment = document.querySelector(".komment");
+const bodyNotShow = document.querySelector("body");
 const pass = 0868;
 let flag = false;
-const userPass = +prompt("Введите пароль");
 
-if (userPass === pass) {
-    flag = true;
+for (let i = 0; i < 1; i++) {
+    bodyNotShow.style.display = "none";
+    const userPass = +prompt("Введите пароль");
+    if (userPass != pass) {
+        i--;
+    }else {
+        bodyNotShow.style.display = "block";
+        flag = true;
+    }
 }
 
 form.addEventListener("submit", (e) => {
@@ -15,12 +22,12 @@ form.addEventListener("submit", (e) => {
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
+    console.log(day);
     const hours = date.getHours();
     const minut = date.getMinutes();
 
     const auditor = e.target[0].value;
     const message = e.target[1].value;
-    // const pass = e.target[2].value;
         if (auditor && message && auditor.length >= 4 && message.length >= 4 ) {
             komment.innerHTML += `
         <div class="sms">
@@ -39,13 +46,53 @@ form.addEventListener("submit", (e) => {
         alert("Дозревай");
     }
 }) 
-const arr = ["sfsf", "agsr", 'ger', "agsr", "agsr",]
+
+const flocksOfSheep = [true, true, true, true, false, true, false, false, true, true, true, true, false];
+
+function numberOfSheep(arr) {
+    const num = []; 
+    arr.forEach(item => {
+        if(item) {
+            num.push(item)
+        }
+    })
+    return `${num.length} овец присутствуют`;
+}
+
+console.log(numberOfSheep(flocksOfSheep));
+const myGame = document.querySelector(".game");
+
+myGame.addEventListener("click", () => {
+    function game() {
+        const K = "Камень";
+        const N = "Ножницы";
+        const B = "Бумага";
+        const playerOne = prompt('playerOne какаой твой выбор "Камень", "Ножницы", "Бумага"');
+        const playerTwo = prompt('playerTwo какаой твой выбор "Камень", "Ножницы", "Бумага"');
+    
+        if (playerOne == K && playerTwo == N || playerOne == N && playerTwo == B || playerOne == B && playerTwo == K) {
+            alert("Победил playerOne");
+        }else if (playerOne == N && playerTwo == K || playerOne == B && playerTwo == N || playerOne == K && playerTwo == B) {
+            alert("Победил playerTwo");
+        }else if (!playerOne || !playerTwo || playerOne != K || playerOne != N || playerOne != B || playerTwo != K || playerTwo != N || playerTwo != B){
+            alert("Это нечестно")
+        }else {
+            alert("Ничья");
+        }
+    }
+    
+    game();
+});
+
+const arr = ["Привет", "Досведания", 'Hello', "Bojure", "Hello", "Hello", "Hello"];
             
-function isAdult(arr, str) {
+const newArr = function(arr, str) {
+    const list = [];
     arr.map(item => {
         if (item == str) {
-            console.log(item);
+            list.push(item);
         }
     });
+    return list;
 }
-isAdult(arr, "agsr");
+console.log(newArr(arr, "Hello"));
